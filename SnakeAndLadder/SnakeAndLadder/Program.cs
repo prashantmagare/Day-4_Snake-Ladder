884f798fb490e8dc955c8e74e8f34609143bd065
prashantmagare <prashantmagare3010@gmail.com>
2022-05-12 22:43:18 +0530

    Added: UC2_Roll the die

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -7,10 +7,12 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             int PlayerPosition = 0;
+            Random random = new Random();
+            int die = random.Next(1, 7);
             {
                 Console.WriteLine("welcome to Snake And Ladder Programe");
                 Console.WriteLine("Starting Position is " + PlayerPosition);
-
+                Console.WriteLine("Dice roll: " + die);
             }
         }
     }
