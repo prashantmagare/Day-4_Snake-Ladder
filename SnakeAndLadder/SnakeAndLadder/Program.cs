fec1d95a5613c3b5d11a8fa4602ed249bd4fa799
prashantmagare <prashantmagare3010@gmail.com>
2022-05-15 16:02:09 +0530

    Added: UC4_Checking win Position Of Player

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
-//UC3 check Position
+//UC4 Win Position
 namespace SnakeAndLadder
 {
     internal class Program
     {
+
         public const int START = 0;
         public const int NO_PLAY = 0;
         public const int LADDER = 1;
         public const int SNAKE = 2;
+        public const int WIN_POSITION = 100;
 
         static void Main(string[] args)
         {
@@ -15,35 +17,42 @@ namespace SnakeAndLadder
             int position = 0;
             int newposition = 0;
             int PlayerPosition = 0;
-
-            Random random = new Random();
-            int dice = random.Next(0, 7);
-            int CheckOption = (random.Next() * 3);
-
-            switch (CheckOption)
+            while (position != WIN_POSITION)
             {
-                case NO_PLAY:
-                    newposition = 0;
-                    break;
-                case LADDER:
-                    newposition = dice;
-                    break;
-                default:
-                case SNAKE:
-                    newposition = -dice;
-                    break;
-            }
-            position = position + newposition;
-            if (position < 0)
-            {
-                position = 0;
-            }
-            Console.WriteLine("position : " + position);
+                //it is using to genetate random numbers
+                Random random = new Random();
+                int dice = random.Next(0, 7);
+                int CheckOption = (random.Next() * 3);
 
-            {
-                Console.WriteLine("welcome to Snake And Ladder Programe");
-                Console.WriteLine("Starting Position is " + PlayerPosition);
-                Console.WriteLine("Dice roll: " + dice);
+                switch (CheckOption)
+                {
+                    case NO_PLAY:
+                        newposition = 0;
+                        break;
+                    case LADDER:
+                        newposition = dice;
+                        break;
+                    default:
+                    case SNAKE:
+                        newposition = -dice;
+                        break;
+                }
+                position = position + newposition;
+                if (position < 0)
+                {
+                    position = 0;
+                }
+                Console.WriteLine("position : " + position);
+
+
+
+
+                {
+                    Console.WriteLine("welcome to Snake And Ladder Programe");
+                    Console.WriteLine("Starting Position is " + PlayerPosition);
+                    Console.WriteLine("Dice roll: " + dice);
+
+                }
             }
         }
     }
