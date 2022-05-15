2796469cb0e5f6b427324dffa02dd721a8e3aaa0
prashantmagare <prashantmagare3010@gmail.com>
2022-05-15 15:41:12 +0530

    Added: UC3_Checking Position Of Player

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -1,18 +1,49 @@
 ﻿using System;
-//UC1 StartPosition
+//UC3 check Position
 namespace SnakeAndLadder
 {
     internal class Program
     {
+        public const int START = 0;
+        public const int NO_PLAY = 0;
+        public const int LADDER = 1;
+        public const int SNAKE = 2;
+
         static void Main(string[] args)
         {
+            //Variables
+            int position = 0;
+            int newposition = 0;
             int PlayerPosition = 0;
+
             Random random = new Random();
-            int die = random.Next(1, 7);
+            int dice = random.Next(0, 7);
+            int CheckOption = (random.Next() * 3);
+
+            switch (CheckOption)
+            {
+                case NO_PLAY:
+                    newposition = 0;
+                    break;
+                case LADDER:
+                    newposition = dice;
+                    break;
+                default:
+                case SNAKE:
+                    newposition = -dice;
+                    break;
+            }
+            position = position + newposition;
+            if (position < 0)
+            {
+                position = 0;
+            }
+            Console.WriteLine("position : " + position);
+
             {
                 Console.WriteLine("welcome to Snake And Ladder Programe");
                 Console.WriteLine("Starting Position is " + PlayerPosition);
-                Console.WriteLine("Dice roll: " + die);
+                Console.WriteLine("Dice roll: " + dice);
             }
         }
     }
